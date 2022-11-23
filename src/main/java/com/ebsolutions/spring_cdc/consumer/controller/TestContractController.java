0f09460fc2e07ee519d023e27e35c0e69f32aeb4
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-11-23 06:23:45 -0500

    Consumer can call producer's even-odd

--- src/main/java/com/ebsolutions/spring_cdc/consumer/controller/TestContractController.java
+++ src/main/java/com/ebsolutions/spring_cdc/consumer/controller/TestContractController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,11 +19,22 @@ public class TestContractController {
     @Autowired
     private ProducerConfig producerConfig;
 
-    @GetMapping()
-    public ResponseEntity<String> callProducerApplication() throws URISyntaxException {
+    @GetMapping("hello")
+    public ResponseEntity<String> callProducerHello() throws URISyntaxException {
 
         RestTemplate restTemplate = new RestTemplate();
         String producerUri = "http://" + producerConfig.getHost() + ":" + producerConfig.getPort() + "/hello";
+        URI uri = new URI(producerUri);
+        String producerResponse = restTemplate.getForObject(uri, String.class);
+
+        return ResponseEntity.status((200)).body(producerResponse);
+    }
+
+    @GetMapping("even-odd")
+    public ResponseEntity<String> callProducerEvenOdd(@RequestParam("number") Integer number) throws URISyntaxException {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String producerUri = "http://" + producerConfig.getHost() + ":" + producerConfig.getPort() + "/even-odd?number=" + number;
         URI uri = new URI(producerUri);
         String producerResponse = restTemplate.getForObject(uri, String.class);
 
